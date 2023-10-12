cfebe886e80d038fb5916aede323b933bbf165e2
DaryllCulas <culasdaryll99@gmail.com>
2023-10-12 16:59:34 +0800

    Add a button

--- First_Flutter_App/flutter_application_1/lib/main.dart
+++ First_Flutter_App/flutter_application_1/lib/main.dart
@@ -27,6 +27,10 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -41,7 +45,7 @@ class MyHomePage extends StatelessWidget {
           Text(appState.current.asLowerCase),
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext();
             },
             child: Text('Next'),
           ),
