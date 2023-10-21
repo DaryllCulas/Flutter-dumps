3783e752d841a1a2e3aab89b78b9f1ebf38d76ab
DaryllCulas <culasdaryll99@gmail.com>
2023-10-22 02:52:05 +0800

    Use switch expression

--- First_Flutter_App/flutter_application_3/lib/main.dart
+++ First_Flutter_App/flutter_application_3/lib/main.dart
@@ -65,14 +65,11 @@ class BlockWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TextStyle? textStyle;
-    switch (block.type) {
-      case 'h1':
-        textStyle = Theme.of(context).textTheme.displayMedium;
-      case 'p' || 'checkbox':
-        textStyle = Theme.of(context).textTheme.bodyMedium;
-      case _:
-        textStyle = Theme.of(context).textTheme.bodySmall;
-    }
+    textStyle = switch (block.type) {
+      'h1' => Theme.of(context).textTheme.displayMedium,
+      'p' || 'checkbox' => Theme.of(context).textTheme.bodyMedium,
+      _ => Theme.of(context).textTheme.bodySmall
+    };
 
     return Container(
       margin: const EdgeInsets.all(8),
