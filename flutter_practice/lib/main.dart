0e8c2311d22f08e7025ea320895fbae70e9eb2d1
DaryllCulas <culasdaryll99@gmail.com>
2024-02-20 14:34:12 +0800

    Add sample scaffold

--- flutter_practice/lib/main.dart
+++ flutter_practice/lib/main.dart
@@ -1,33 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/scaffold_concepts.dart';
 
-// function to trigger build process
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Container Example"),
+          title: const Text('Hello World'),
         ),
-        body: Container(
-          height: 200,
-          width: double.infinity,
-          // color: Colors.green,
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.all(50),
-          alignment: Alignment.bottomCenter,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 3),
-          ),
-          transform: Matrix4.rotationZ(0.1),
-          child: const Text(
-            "Hello I am inside a container",
-            style: TextStyle(fontSize: 20),
+        body: const Center(
+          child: Text(
+            "Welcome to the world!!!",
+            style: TextStyle(color: Colors.black, fontSize: 40.0),
           ),
         ),
       ),
