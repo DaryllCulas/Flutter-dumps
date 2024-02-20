509193214c3789a8c9096f30ac88fa67435a38c3
DaryllCulas <culasdaryll99@gmail.com>
2024-02-21 02:04:31 +0800

    Add shopping cart

--- flutter_practice/lib/main.dart
+++ flutter_practice/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/sample_widgets/drawer_sample.dart';
+// import 'package:flutter_practice/sample_widgets/drawer_sample.dart';
+import 'package:flutter_practice/sample_widgets/shopping_list.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,8 +11,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: DrawerSample(), // Change here
+      // home: Scaffold(
+      //   body: DrawerSample(), // Change here
+      // ),
+      home: ShoppingList(
+        products: [
+          Product(name: 'Eggs'),
+          Product(name: 'Flour'),
+          Product(name: 'Chocolate'),
+        ],
       ),
     );
   }
