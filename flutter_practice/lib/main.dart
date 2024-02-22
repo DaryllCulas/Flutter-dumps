70eed8a2e634cdbfad647f007311bddecfdcb0e8
DaryllCulas <culasdaryll99@gmail.com>
2024-02-23 07:28:39 +0800

    Add animatedIcon

--- flutter_practice/lib/main.dart
+++ flutter_practice/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/sample_widgets/animated_text_style_default.widget.dart';
+import 'package:flutter_practice/sample_widgets/animated_icon_widget.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
           ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.blueGrey),
       debugShowCheckedModeBanner: false,
       home: const Scaffold(
-        body: AnimatedDefaultText(), // Change here
+        body: AnimatedIconHere(), // Change here
       ),
     );
   }
---
+++ flutter_practice/lib/sample_widgets/animated_icon_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AnimatedIconHere extends StatefulWidget {
+  const AnimatedIconHere({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedIconHere> createState() => _AnimatedIconHereState();
+}
+
+class _AnimatedIconHereState extends State<AnimatedIconHere>
+    with TickerProviderStateMixin {
+  bool _isPlay = false;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          if (_isPlay == false) {
+            _controller.forward();
+            _isPlay = true;
+          } else {
+            _controller.reverse();
+            _isPlay = false;
+          }
+        },
+        child: AnimatedIcon(
+          icon: AnimatedIcons.play_pause,
+          progress: _controller,
+          size: 100,
+        ),
+      ),
+    );
+  }
+}
